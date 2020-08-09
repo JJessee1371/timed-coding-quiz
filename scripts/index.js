0693fad115f98b95168cce4b85dee1376f8ef511
Jon Jessee <tjessee7624@gmail.com>
2020-08-09 11:46:30 -0600

    Set event listener for start button and set up timer for basic functionality.

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 let minutesDisplay = document.getElementById('minutes-display');
 let secondsDisplay = document.getElementById('seconds-display');
+let startButton = document.getElementById('begin');
 
 
-function (startTimer() {
-    clearInterval(interval);
-})
+
+function startTimer() {
+    let totalMinutes = 5;
+    let totalSeconds = totalMinutes * 60;
+
+    const interval = setInterval( function() {
+        totalSeconds--;
+        secondsDisplay.textContent = totalSeconds;
+
+        if (totalSeconds <= 0) {
+            clearInterval(interval);
+        };
+    }, 1000);
+
+};
+
+
+
+
+
+startButton.addEventListener('click', startTimer);
 
     
